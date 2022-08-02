627e70d16332853c6ac9c6df2592f31d637cb96f
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-08-02 08:35:35 -0300

    feat: list product

---
+++ src/modules/products/services/ListProductService copy.ts
@@ -0,0 +1,26 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Products';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+}
+
+class GetProductService {
+  public async execute({ id }: IRequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = productsRepository.findOne({
+      where: { id },
+    });
+
+    if (!product) {
+      throw new AppError('Product not found!');
+    }
+
+    return product;
+  }
+}
+
+export default GetProductService;
