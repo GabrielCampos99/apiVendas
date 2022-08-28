a06944f0712cdffd5e910843efaaa42dd3df9111
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-08-28 14:17:37 -0300

    create user

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,6 @@
 import AppError from '@shared/errors/AppError';
+import bcrypt from 'bcrypt';
+
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -18,10 +20,12 @@ class CreateUserService {
       throw new AppError('Email address already used.');
     }
 
+    const hashedPassword = await bcrypt.hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
